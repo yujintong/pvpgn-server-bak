e520dbb1045e129db830f0c10ea3f79fd3320512
RElesgoe <RElesgoe@users.noreply.github.com>
2020-11-11 14:18:06 -0800

    Move function pointer definitions to d2ptrs.cpp

--- src/d2warden/d2callstub.h
+++ src/d2warden/d2callstub.h
@@ -5,3 +5,9 @@ DWORD __fastcall D2GAME_LeaveCriticalSection_STUB(DWORD unk);
 DWORD __fastcall D2GAME_Send0XAEPacket_STUB(void* ptPlayer, DWORD Length, DWORD* Packet);
 DWORD __fastcall D2GAME_GameFindUnitFunc_STUB(DWORD ptGame, DWORD dwUnitId, DWORD dwUnitType);
 DWORD __fastcall D2GAME_SendPacket_STUB(char* ptConnection, unsigned char* Packet, DWORD PacketLength);
+
+#define D2GAME_GetClient			D2GAME_GetClient_STUB
+#define D2GAME_LeaveCriticalSection	D2GAME_LeaveCriticalSection_STUB
+#define D2GAME_Send0XAEPacket		D2GAME_Send0XAEPacket_STUB
+#define D2GAME_SendPacket			D2GAME_SendPacket_STUB
+#define D2GAME_GameFindUnitFunc		D2GAME_GameFindUnitFunc_STUB
