b9538b1c5dc3e8e8e9230b92240f739cab851fe7
relesgoe <RElesgoe@users.noreply.github.com>
2020-12-08 21:21:50 -0800

    Call conn_increment_passfail_count() for current bot connection in handle_bot_packet()

--- src/bnetd/handle_bot.cpp
+++ src/bnetd/handle_bot.cpp
@@ -245,6 +245,7 @@ namespace pvpgn
 						if (hash_eq(trypasshash1, oldpasshash1) != 1)
 						{
 							eventlog(eventlog_level_info, __FUNCTION__, "[{}] bot login for \"{}\" refused (wrong password)", conn_get_socket(c), loggeduser);
+							conn_increment_passfail_count(c);
 							conn_set_state(c, conn_state_bot_username);
 
 							if (!(rpacket = packet_create(packet_class_raw)))
